75b958a93a878672d4393bf7242b071aa1c3fabe
Harry Lanz <harry.lanz@proton.me>
2026-04-13 23:17:39 +0100

    disable modeline

--- nvim/.config/nvim/lua/core/options.lua
+++ nvim/.config/nvim/lua/core/options.lua
@@ -70,6 +70,7 @@ vim.opt.runtimepath:remove("/usr/share/vim/vimfiles") -- remove vim plugins from
 vim.opt.autoread = true -- auto-reload files changed on disk (works with checktime)
 vim.opt.smoothscroll = true -- smooth scolling if version supports it
 vim.opt.laststatus = 2 -- always show statusline (maybe not useful if lualine for example)
+vim.opt.modeline = false -- disable modeline for no arbitrary code exec
 
 -- diagnostic settings
 vim.diagnostic.config({
