267aad42192f8bd817585f0e636411ab160ea2a0
Harry Lanz <harry.lanz@proton.me>
2026-06-09 18:45:04 +0100

    remove redundant files and update visual options

--- nvim/.config/nvim/lua/core/options.lua
+++ nvim/.config/nvim/lua/core/options.lua
@@ -3,11 +3,11 @@ vim.opt.number = true -- make line numbers visible
 vim.opt.relativenumber = true -- set line numbers to be relative to current line
 vim.opt.colorcolumn = "80,100" -- column to show where 80 character limit is
 vim.opt.cursorline = true -- highlight line the cursor is on
-vim.opt.hlsearch = false -- show search results as highlighted
+vim.opt.hlsearch = true -- true: show all search results from highlights (clear with keymap? or set to false to only show one at a time)
 vim.opt.showmode = false -- show what interaction mode vim is in (set to false if statusline plugin installed)
 vim.opt.ruler = false -- disable line and col number as statusline will show this
-vim.opt.laststatus = 3 -- show statusline globally rather than per window
-vim.opt.numberwidth = 4 -- width of number column
+vim.opt.laststatus = 3 -- show statusline globally rather than per window (need statusline plugin for this to be relevant)
+vim.opt.numberwidth = 4 -- width of number gutter column
 vim.opt.showtabline = 1 -- 0 never show, 1 show when multiple tabs, 2 always show
 vim.opt.signcolumn = "yes:1" -- keep sign column
 vim.opt.conceallevel = 0 -- make markdown text symbols visible
@@ -15,11 +15,12 @@ vim.opt.cmdheight = 1 -- height of command line in editor
 vim.opt.pumheight = 10 -- set popup menu height
 vim.opt.pumblend = 0 -- no transparancy for popup menus
 vim.opt.winblend = 0 -- no transparency for floating windows
-vim.opt.completeopt = "menuone,noselect" -- show completion even for one item, don't auto insert
+-- vim.opt.completeopt = "menuone,noselect" -- show completion even for one item, don't auto insert, perhaps not needed when using completion plugins?
 vim.opt.shortmess:append("c") -- suppress completion messages for quieter UI
 vim.opt.list = true -- visualise whitespace
 vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
 vim.opt.winborder = "rounded"
+vim.opt.termguicolors = true -- enable 24 bit colour
 
 -- text editing behavioural options
 vim.opt.wrap = false -- prevent line wrapping
@@ -45,7 +46,6 @@ vim.opt.ignorecase = true -- ignore case in search unless \C or capital in searc
 vim.opt.smartcase = true -- paired with ignorecase to give correct functionality as described above
 vim.opt.scrolloff = 8 -- keep this many lines above and below cursor vertically
 vim.opt.sidescrolloff = 8 -- keep this many columns visible either side of cursor horizontally
-vim.opt.termguicolors = true -- enable 24 bit colour
 vim.opt.splitbelow = true -- horizontal splits are placed below by default
 vim.opt.splitright = true -- vertical splits are placed right by default
 vim.opt.equalalways = true -- keep splits balanced when opening and closing
