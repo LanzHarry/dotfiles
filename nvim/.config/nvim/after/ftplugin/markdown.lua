2d6322a062b33743aa4f7d3dce7c0862f40ff423
Harry Lanz <harry.lanz@proton.me>
2026-06-10 12:57:35 +0100

    set options according to preferences and redundancy review, add markdown specific options for overriding

---
+++ nvim/.config/nvim/after/ftplugin/markdown.lua
@@ -0,0 +1,10 @@
+-- settings for markdown .md files
+local set = vim.opt_local
+
+set.wrap = true
+set.linebreak = true -- lines will not wrap mid word
+set.breakindent = true -- wrapped lines will match indent of original line
+
+set.spell = true
+set.spellland = "en_gb"
+
--- nvim/.config/nvim/lua/core/options.lua
+++ nvim/.config/nvim/lua/core/options.lua
@@ -16,7 +16,7 @@ vim.opt.pumheight = 10 -- set popup menu height
 vim.opt.pumblend = 0 -- no transparancy for popup menus
 vim.opt.winblend = 0 -- no transparency for floating windows
 -- vim.opt.completeopt = "menuone,noselect" -- show completion even for one item, don't auto insert, perhaps not needed when using completion plugins?
-vim.opt.shortmess:append("c") -- suppress completion messages for quieter UI
+-- vim.opt.shortmess:append("c") -- suppress completion messages for quieter UI
 vim.opt.list = true -- visualise whitespace
 vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
 vim.opt.winborder = "rounded"
@@ -25,23 +25,23 @@ vim.opt.termguicolors = true -- enable 24 bit colour
 -- text editing behavioural options
 vim.opt.wrap = false -- prevent line wrapping
 vim.opt.autoindent = true -- indent new line same as current line
-vim.opt.smartindent = false -- inbuilt smart indent control
+vim.opt.smartindent = false -- inbuilt smart indent control (legacy c style indentation)
 vim.opt.breakindent = true -- wrapped lines will match indent of original line
-vim.opt.linebreak = false -- soft linewrapping (only relevant when wrap is true)
+vim.opt.linebreak = true -- soft linewrapping (only relevant when wrap is true)
 vim.opt.shiftwidth = 4 -- number of spaces for an indentation
 vim.opt.tabstop = 4 -- number of spaces for a tab
 vim.opt.softtabstop = 4 -- number of spaces a tab counts for whilst editing
 vim.opt.expandtab = true -- turn tab into spaces
 vim.opt.smarttab = true -- tab line at start uses shiftwidth to correctly indent
 vim.opt.shiftround = true -- restrict to multiples of shiftwidth
-vim.opt.whichwrap = "bs<>[]hl" -- which cursor keys can move cursor across lines at start/end
+vim.opt.whichwrap = "bs<>[]" -- which cursor keys can move cursor across lines at start/end
 vim.opt.backspace = "indent,eol,start" -- allow backspace for indents and start/end of lines
 
 -- interaction behavioural settings
 vim.opt.mouse = "a" -- enable mouse mode in all modes
 vim.schedule(function()
   vim.opt.clipboard = "unnamedplus"
-end) -- sync clipboard between OS clipboard and neovim registers
+end) -- sync clipboard between OS clipboard and neovim registers, deferred to avoid startup issues
 vim.opt.ignorecase = true -- ignore case in search unless \C or capital in search
 vim.opt.smartcase = true -- paired with ignorecase to give correct functionality as described above
 vim.opt.scrolloff = 8 -- keep this many lines above and below cursor vertically
@@ -57,32 +57,32 @@ vim.opt.inccommand = "split" -- live preview for :substitute
 -- backend settings
 vim.opt.swapfile = false -- whether to create a swapfile
 vim.opt.backup = false -- whether to create backups
-vim.opt.writebackup = false -- whether to write directly to disk
+vim.opt.writebackup = false -- disable temporary backup during write
 vim.opt.undofile = true -- save undo history
--- vim.opt.undodir = nil -- uses default in state dir
-vim.opt.undolevels = 10000 -- max undo steps
+-- undodir defaults to ~/.local/state/nvim/undo, no override needed
+vim.opt.undolevels = 2000 -- max undo steps
 vim.opt.undoreload = 10000 -- lines to save for reload
 vim.opt.fileencoding = "utf-8" -- default file encoding
 vim.opt.updatetime = 250 -- wait time for neovim to fire certain events
 vim.opt.timeoutlen = 300 -- wait time to complete mapped sequence
 vim.opt.iskeyword:append("-") -- treat hyphenated words as one word
 vim.opt.runtimepath:remove("/usr/share/vim/vimfiles") -- remove vim plugins from neovim path
-vim.opt.autoread = true -- auto-reload files changed on disk (works with checktime)
-vim.opt.smoothscroll = true -- smooth scolling if version supports it
-vim.opt.laststatus = 2 -- always show statusline (maybe not useful if lualine for example)
+vim.opt.autoread = true -- auto-reload files changed on disk (works with checktime autocommand)
+vim.opt.smoothscroll = true -- smooth scrolling if version supports it
 vim.opt.modeline = false -- disable modeline for no arbitrary code exec
 
 -- diagnostic settings
 vim.diagnostic.config({
-  virtual_text = {
-    severity = { min = vim.diagnostic.severity.WARN },
+  virtual_text = false,
+  virtual_lines = {
+    current_line = true,
+    -- min = vim.diagnostic.severity.WARN,
   },
-  virtual_lines = false,
   signs = true,
   underline = true,
   severity_sort = true,
   update_in_insert = false,
-  float = { border = "rounded", source = true },
+  float = { source = true },
   jump = { float = true },
 })
 
