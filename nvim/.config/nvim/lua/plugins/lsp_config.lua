9a2c6cd9d2d38045adab79b9e5c8e948b5045980
Harry Lanz <harry.lanz@proton.me>
2026-06-16 11:14:31 +0100

    note hint config details

--- nvim/.config/nvim/lua/plugins/lsp_config.lua
+++ nvim/.config/nvim/lua/plugins/lsp_config.lua
@@ -28,6 +28,8 @@ vim.api.nvim_create_autocmd("LspAttach", {
     end
 
     if client and client:supports_method("textDocument/inlayHint", ev.buf) then
+      -- inlay hints must be enabled in .luarc.json or other settings, can also be configured
+      -- to show more or less detail such as types etc.
       vim.lsp.inlay_hint.enable(true, { bufnr = ev.buf })
       map("n", "<leader>th", function()
         vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = ev.buf }))
