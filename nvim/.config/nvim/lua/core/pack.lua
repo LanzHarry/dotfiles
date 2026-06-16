fa90f70491de279f41428c22e59d0d4a385cc3f5
Harry Lanz <harry.lanz@proton.me>
2026-06-16 21:49:05 +0100

    add treesitter with minimal config

--- nvim/.config/nvim/lua/core/pack.lua
+++ nvim/.config/nvim/lua/core/pack.lua
@@ -2,3 +2,24 @@ require("plugins.colourscheme")
 require("plugins.completion")
 require("plugins.format")
 require("plugins.lsp_config")
+require("plugins.treesitter")
+
+vim.api.nvim_create_autocmd("PackChanged", {
+  group = vim.api.nvim_create_augroup("PackChangedGroup", { clear = true }),
+  callback = function(ev)
+    local name = ev.data.spec.name
+    local kind = ev.data.kind
+
+    if kind ~= "install" and kind ~= "update" then
+      return
+    end
+
+    if name == "nvim-treesitter" then
+      if not ev.data.active then
+        vim.cmd.packadd("nvim-treesitter")
+      end
+      vim.cmd("TSUpdate")
+      return
+    end
+  end,
+})
--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,36 +1,49 @@
-return {
-  "nvim-treesitter/nvim-treesitter",
-  event = { "BufReadPre", "BufNewFile" },
-  build = ":TSUpdate",
-  opts = {
-    ensure_installed = {
-      "bash",
-      "c",
-      "lua",
-      "python",
-      "vim",
-      "vimdoc",
-      "query",
-      "markdown",
-      "markdown_inline",
-    },
-    auto_install = true,
-    prefer_git = true,
-    highlight = {
-      enable = true,
-      -- Disable for large files (performance safeguard)
-      disable = function(_, buf)
-        local max_filesize = 200 * 1024 -- 200 KB
-        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-        return ok and stats and stats.size > max_filesize
-      end,
-      additional_vim_regex_highlighting = false,
-    },
-    textobjects = {
-      select = {
-        enable = false,
-      },
-    },
-    indent = { enable = true },
+vim.pack.add({
+  {
+    src = "https://github.com/nvim-treesitter/nvim-treesitter",
+    name = "nvim-treesitter",
+    version = "main",
   },
-}
+})
+
+-- awaiting proper config, copy kickstart style defaults and autocmds
+-- treesitter highlighting is powerful, as well as smarter text objects
+-- and also incremental selection. Indentation is also a feature however this
+-- is experimental
+require("nvim-treesitter").setup({})
+-- return {
+--   "nvim-treesitter/nvim-treesitter",
+--   event = { "BufReadPre", "BufNewFile" },
+--   build = ":TSUpdate",
+--   opts = {
+--     ensure_installed = {
+--       "bash",
+--       "c",
+--       "lua",
+--       "python",
+--       "vim",
+--       "vimdoc",
+--       "query",
+--       "markdown",
+--       "markdown_inline",
+--     },
+--     auto_install = true,
+--     prefer_git = true,
+--     highlight = {
+--       enable = true,
+--       -- Disable for large files (performance safeguard)
+--       disable = function(_, buf)
+--         local max_filesize = 200 * 1024 -- 200 KB
+--         local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+--         return ok and stats and stats.size > max_filesize
+--       end,
+--       additional_vim_regex_highlighting = false,
+--     },
+--     textobjects = {
+--       select = {
+--         enable = false,
+--       },
+--     },
+--     indent = { enable = true },
+--   },
+-- }
