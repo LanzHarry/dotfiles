f16f2d72d11eaa3116f7757582c580217cbb4088
Harry Lanz <harry.lanz@proton.me>
2026-06-16 11:11:15 +0100

    enable inlay hints in luarc

--- nvim/.config/nvim/lua/plugins/lsp_config.lua
+++ nvim/.config/nvim/lua/plugins/lsp_config.lua
@@ -10,37 +10,37 @@ end
 
 vim.api.nvim_create_autocmd("LspAttach", {
   group = augroup("DefaultLspAttach", { clear = true }),
-  callback = function(event)
-    local client = vim.lsp.get_client_by_id(event.data.client_id)
-    if client and client:supports_method("textDocument/documentHighlight", event.buf) then
+  callback = function(ev)
+    local client = vim.lsp.get_client_by_id(ev.data.client_id)
+    if client and client:supports_method("textDocument/documentHighlight", ev.buf) then
       local highlight_augroup = augroup("LspHighlightGroup", { clear = false })
       vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-        buffer = event.buf,
+        buffer = ev.buf,
         group = highlight_augroup,
         callback = vim.lsp.buf.document_highlight,
       })
 
       vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
-        buffer = event.buf,
+        buffer = ev.buf,
         group = highlight_augroup,
         callback = vim.lsp.buf.clear_references,
       })
     end
 
-    if client and client:supports_method("textDocument/inlayHint", event.buf) then
-      vim.lsp.inlay_hint.enable(true, { bufnr = event.buf })
+    if client and client:supports_method("textDocument/inlayHint", ev.buf) then
+      vim.lsp.inlay_hint.enable(true, { bufnr = ev.buf })
       map("n", "<leader>th", function()
-        vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf }))
-      end, "Toggle inlay hints", { buffer = event.buf })
+        vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = ev.buf }))
+      end, "Toggle inlay hints", { buffer = ev.buf })
     end
   end,
 })
 
 vim.api.nvim_create_autocmd("LspDetach", {
   group = augroup("DefaultLspDetach", { clear = true }),
-  callback = function(event)
+  callback = function(ev)
     vim.lsp.buf.clear_references()
-    vim.api.nvim_clear_autocmds({ group = "LspHighlightGroup", buffer = event.buf })
+    vim.api.nvim_clear_autocmds({ group = "LspHighlightGroup", buffer = ev.buf })
   end,
 })
 
