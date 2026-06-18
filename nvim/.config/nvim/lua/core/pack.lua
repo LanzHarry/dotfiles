b7e6637f69313b2dceeab277374603d01d89df2b
Harry Lanz <harry.lanz@proton.me>
2026-06-18 17:55:04 +0100

    add mini icons as first mini module, can now be used to mock web devicons

--- nvim/.config/nvim/lua/core/pack.lua
+++ nvim/.config/nvim/lua/core/pack.lua
@@ -1,9 +1,14 @@
+-- plugins that other plugins depend on
+require("plugins.mini")
+
+-- other plugins do not need synchronous requirements
 require("plugins.colourscheme")
 require("plugins.completion")
 require("plugins.format")
 require("plugins.lsp_config")
 require("plugins.treesitter")
 
+-- on pack changed event make sure any necessary external builds / commands are run
 vim.api.nvim_create_autocmd("PackChanged", {
   group = vim.api.nvim_create_augroup("PackChangedGroup", { clear = true }),
   callback = function(ev)
--- nvim/.config/nvim/lua/plugins/mini.lua
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -1,15 +1,21 @@
-return {
-  {
-    "nvim-mini/mini.nvim",
-    version = false,
-    lazy = false,
-    priority = 900,
-    config = function()
-      require("mini.ai").setup() -- extend and enhance text object motions
-      require("mini.bufremove").setup() -- sane buffer deletion
-      require("mini.icons").setup() -- icon provider
-      require("mini.statusline").setup({ use_icons = true }) -- simple status line
-      require("mini.surround").setup() -- surround actions
-    end,
-  },
-}
+vim.pack.add({ "https://github.com/nvim-mini/mini.nvim" })
+
+local mini_icons = require("mini.icons")
+mini_icons.setup()
+mini_icons.mock_nvim_web_devicons()
+
+-- return {
+--   {
+--     "nvim-mini/mini.nvim",
+--     version = false,
+--     lazy = false,
+--     priority = 900,
+--     config = function()
+--       require("mini.ai").setup() -- extend and enhance text object motions
+--       require("mini.bufremove").setup() -- sane buffer deletion
+--       require("mini.icons").setup() -- icon provider
+--       require("mini.statusline").setup({ use_icons = true }) -- simple status line
+--       require("mini.surround").setup() -- surround actions
+--     end,
+--   },
+-- }
