7a1ef83d10a9c5e14eb9b92430db0fdaddfc168e
Harry Lanz <harry.lanz@proton.me>
2026-06-19 12:50:29 +0100

    add minimal telescope config before telescope customisation

--- nvim/.config/nvim/lua/core/pack.lua
+++ nvim/.config/nvim/lua/core/pack.lua
@@ -1,3 +1,6 @@
+-- initialise pack changed checks before any plugins are required
+require("core.pack_changed")
+
 -- plugins that other plugins depend on
 require("plugins.mini")
 
@@ -6,25 +9,5 @@ require("plugins.colourscheme")
 require("plugins.completion")
 require("plugins.format")
 require("plugins.lsp_config")
+require("plugins.telescope")
 require("plugins.treesitter")
-
--- on pack changed event make sure any necessary external builds / commands are run
-vim.api.nvim_create_autocmd("PackChanged", {
-  group = vim.api.nvim_create_augroup("PackChangedGroup", { clear = true }),
-  callback = function(ev)
-    local name = ev.data.spec.name
-    local kind = ev.data.kind
-
-    if kind ~= "install" and kind ~= "update" then
-      return
-    end
-
-    if name == "nvim-treesitter" then
-      if not ev.data.active then
-        vim.cmd.packadd("nvim-treesitter")
-      end
-      vim.cmd("TSUpdate")
-      return
-    end
-  end,
-})
