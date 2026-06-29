18d1630711b67d44a633f8e409243a5cef1a1e22
Harry Lanz <harry.lanz@proton.me>
2026-06-29 12:34:44 +0100

    Add mini ai, pairs, and surround

--- nvim/.config/nvim/lua/plugins/mini.lua
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -8,6 +8,7 @@ mini_icons.setup()
 mini_icons.mock_nvim_web_devicons()
 
 local mini_bufremove = require("mini.bufremove")
+mini_bufremove.setup()
 map("n", "<leader>bx", function()
   mini_bufremove.delete(0, false)
 end, "Close buffer")
@@ -16,6 +17,15 @@ local mini_statusline = require("mini.statusline")
 mini_statusline.setup({
   use_icons = true,
 })
+
+local mini_ai = require("mini.ai")
+mini_ai.setup()
+
+local mini_pairs = require("mini.pairs")
+mini_pairs.setup()
+
+local mini_surround = require("mini.surround")
+mini_surround.setup()
 
 -- todo: add ai, pairs, and surround
 
