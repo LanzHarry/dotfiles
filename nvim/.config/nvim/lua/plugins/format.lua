51cfa3e85906ae17d98ea9e0ff589d41d10b49da
Harry Lanz <harry.lanz@proton.me>
2026-07-02 11:44:59 +0100

    Add rumdl as markdown formatter and remove old commented config

--- nvim/.config/nvim/lua/plugins/format.lua
+++ nvim/.config/nvim/lua/plugins/format.lua
@@ -7,6 +7,7 @@ require("conform").setup({
   formatters_by_ft = {
     lua = { "stylua" },
     python = { "ruff_format" },
+    markdown = { "rumdl" },
   },
 })
 
@@ -33,53 +34,3 @@ vim.api.nvim_create_autocmd("BufWritePre", {
     })
   end,
 })
-
--- -- filetypes to disable formatting for
--- local disable_filetypes = {}
---
--- return {
---   "stevearc/conform.nvim",
---   event = { "BufReadPre", "BufNewFile" },
---   opts = {
---     notify_on_error = false,
---     formatters_by_ft = {
---       c = { "clang_format" },
---       cpp = { "clang_format" },
---       css = { "prettierd" },
---       h = { "clang_format" },
---       html = { "prettierd" },
---       javascript = { "prettierd" },
---       json = { "prettierd" },
---       lua = { "stylua" },
---       python = { "ruff_format" },
---       sql = { "sql_formatter" },
---       typescript = { "prettierd" },
---       typescriptreact = { "prettierd" },
---       yaml = { "prettierd" },
---     },
---     format_on_save = function(bufnr)
---       if disable_filetypes[vim.bo[bufnr].filetype] then
---         return nil
---       else
---         return {
---           timeout_ms = 2000,
---           lsp_format = "never",
---         }
---       end
---     end,
---   },
---   keys = {
---     {
---       "<leader>f",
---       function()
---         require("conform").format({
---           async = true,
---           lsp_format = "never",
---           timeout_ms = 2000,
---         })
---       end,
---       mode = { "n", "v" },
---       desc = "[F]ormat buffer",
---     },
---   },
--- }
